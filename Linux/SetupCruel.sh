28c90d23464f714a37351dec87f01f108c2e06d1
Dimserene <raul0520@google.com>
2024-08-27 15:18:14 +0800

    2024-08-27a

---
+++ Linux/SetupCruel.sh
@@ -0,0 +1,3 @@
+git clone --recurse-submodules --remote-submodules https://github.com/Dimserene/Cruel-Pack
+cd Cruel-Pack
+cp -r ./Mods /home/$USER/.steam/steam/steamapps/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro
---
+++ Windows/SetupCruel.bat
@@ -0,0 +1,3 @@
+git clone --recurse-submodules --remote-submodules https://github.com/Dimserene/Cruel-Pack
+xcopy "Cruel-Pack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+pause
